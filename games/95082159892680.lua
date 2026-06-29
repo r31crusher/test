502b34e9267392b07e004f240b6d12cbd7f2ab1e
r31crusher <richeypeavler@gmail.com>
2026-06-29 16:30:27 -0400

    Update 95082159892680.lua

--- games/95082159892680.lua
+++ games/95082159892680.lua
@@ -45,15 +45,15 @@ return function(section)
         table.clear(_stage9Bridges)
 
         local PAIRS = {
-            { Vector3.new(-1248, 299, 1468), Vector3.new(-1342, 277, 1466) },
-            { Vector3.new(-1510, 331, 1466), Vector3.new(-1563, 316, 1466) },
-            { Vector3.new(-1627, 316, 1464), Vector3.new(-1740, 285, 1464) },
-            { Vector3.new(-1864, 313, 1465), Vector3.new(-1934, 302, 1465) },
-            { Vector3.new(-2046, 302, 1465), Vector3.new(-2127, 302, 1466) },
-            { Vector3.new(-2183, 321, 1466), Vector3.new(-2244, 309, 1466) },
-            { Vector3.new(-2345, 320, 1466), Vector3.new(-2409, 317, 1466) },
-            { Vector3.new(-2530, 317, 1466), Vector3.new(-2594, 289, 1479) },
-            { Vector3.new(-2788, 303, 1464), Vector3.new(-2853, 278, 1464) },
+            { Vector3.new(-1248, 294, 1468), Vector3.new(-1342, 272, 1466) },
+            { Vector3.new(-1510, 326, 1466), Vector3.new(-1563, 311, 1466) },
+            { Vector3.new(-1627, 311, 1464), Vector3.new(-1740, 280, 1464) },
+            { Vector3.new(-1864, 308, 1465), Vector3.new(-1934, 297, 1465) },
+            { Vector3.new(-2046, 297, 1465), Vector3.new(-2127, 297, 1466) },
+            { Vector3.new(-2183, 316, 1466), Vector3.new(-2244, 304, 1466) },
+            { Vector3.new(-2345, 315, 1466), Vector3.new(-2409, 312, 1466) },
+            { Vector3.new(-2530, 312, 1466), Vector3.new(-2594, 284, 1479) },
+            { Vector3.new(-2788, 298, 1464), Vector3.new(-2853, 273, 1464) },
         }
 
         for _, pair in PAIRS do
