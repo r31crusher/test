27e4b9d86f520eb985bce2f923aa8c8bee1a494c
r31crusher <richeypeavler@gmail.com>
2026-06-29 23:41:15 -0400

    Create 109578395590751.lua

---
+++ games/109578395590751.lua
@@ -0,0 +1,46 @@
+-- +1 Speed Brick Escape (PlaceId 109578395590751)
+
+return function(section)
+    local elements = getgenv()._astroElements
+    local RS = game:GetService("RunService")
+    local plr = game:GetService("Players").LocalPlayer
+
+    local _farming = false
+
+    local function flyTo(pos)
+        while _farming and (plr.Character.HumanoidRootPart.Position - pos).Magnitude > 2 do
+            local speed = plr.Character.Humanoid.WalkSpeed
+            local dir = (pos - plr.Character.HumanoidRootPart.Position).Unit
+            plr.Character.HumanoidRootPart.AssemblyLinearVelocity = dir * speed
+            task.wait()
+        end
+        plr.Character.HumanoidRootPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+    end
+
+    elements:Toggle("World 1 Autofarm", section, function(v)
+        _farming = v
+        if not v then return end
+
+        local noclip = RS.Heartbeat:Connect(function()
+            if plr.Character then
+                for _, p in plr.Character:GetDescendants() do
+                    if p:IsA("BasePart") then
+                        p.CanCollide = false
+                    end
+                end
+            end
+        end)
+
+        task.spawn(function()
+            while _farming do
+                flyTo(Vector3.new(5129, 699, -2559))
+                task.wait(1)
+            end
+            noclip:Disconnect()
+        end)
+    end)
+
+    section.AncestorRemoving:Connect(function()
+        _farming = false
+    end)
+end
